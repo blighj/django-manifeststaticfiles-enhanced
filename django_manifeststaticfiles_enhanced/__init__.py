892ceb1bd527ecf9b17c01e42cbcb7756bb5a1e0
James Bligh <blighj@users.noreply.github.com>
2025-07-23 18:41:24 +0100

    Update version to 0.4.3

--- django_manifeststaticfiles_enhanced/__init__.py
+++ django_manifeststaticfiles_enhanced/__init__.py
@@ -5,7 +5,7 @@ Enhanced ManifestStaticFilesStorage for Django with improvements from
 Django tickets: 27929, 21080, 26583, 28200, 34322
 """
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 from .storage import EnhancedManifestStaticFilesStorage
 
